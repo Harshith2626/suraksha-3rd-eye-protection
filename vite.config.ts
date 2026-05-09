9737519541411142365b12d5bae38c47b1f51d46
Harshith H <1nt24cs104.harshith@nmit.ac.in>
2026-05-09 22:12:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,10 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: {
-      allowedHosts:true,
+      allowedHosts: true,
     },
 
     preview:{
-      allowedHosts:true,
+      allowedHosts: true,
   },
-});
+}});
